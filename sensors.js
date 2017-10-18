dcdf0900b1f3fbe6612a787abca35f1517eece5c
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-10-18 19:42:14 +0300

    comentarii rotatii 2

--- sensors.js
+++ sensors.js
@@ -3,8 +3,8 @@ window.addEventListener('deviceorientation', ondeviceorientation);
 //--------------------------------------------------------------
 function ondeviceorientation(event) 
 { 
-	document.getElementById("id_alpha").innerHTML='Plan orizontal - stanga-dreapta'+Math.round(event.alpha * 100) / 100;
-	document.getElementById("id_beta").innerHTML='Plan vertical - inainte si inapoi'+Math.round(event.beta * 100) / 100;
-	document.getElementById("id_gamma").innerHTML='Plan vertical - stanga-dreapta-'+Math.round(event.gamma * 100) / 100;
+	document.getElementById("id_alpha").innerHTML=Math.round(event.alpha * 100) / 100;
+	document.getElementById("id_beta").innerHTML=Math.round(event.beta * 100) / 100;
+	document.getElementById("id_gamma").innerHTML=Math.round(event.gamma * 100) / 100;
 }
 //--------------------------------------------------------------
