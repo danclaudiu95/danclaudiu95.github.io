f8be893a5413c676fe79b1c91b9919def6507af4
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-10-25 19:41:23 +0300

    Variabila gamma

--- senzori.js
+++ senzori.js
@@ -14,7 +14,7 @@ function ondevicemotion(event)
 	document.getElementById("id_acc").innerHTML = "Acc = " + Math.round(event.acceleration.x * 10) / 10 + " " + Math.round(event.acceleration.y * 10) / 10 + " " + Math.round(event.acceleration.z * 10) / 10;
 	
 	var ag = event.accelerationIncludingGravity;
-	var gama = -Math.atan(ag.x / ag.z) * 180 / Math.PI;
+	var gamma = -Math.atan(ag.x / ag.z) * 180 / Math.PI;
 	var beta = Math.atan(ag.y / ag.z) * 180 / Math.PI;
 	
 	document.getElementById("id_acc_g").innerHTML ="Acc_g = " + Math.round(ag.x * 10) / 10 + " " + Math.round(ag.y * 10) / 10 + " " + Math.round(ag.z * 10) / 10 + "Gama = " + Math.round(gamma * 10) / 10 + " Beta = " + Math.round(beta * 10) / 10;		
