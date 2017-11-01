591a41c2c010e292684bc3988a3cb34d285513c8
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-01 18:18:14 +0200

    Desenare cerc in functie de senzori

--- senzori.js
+++ senzori.js
@@ -1,16 +1,44 @@
-document.getElementById("id_logic_level_version").innerHTML="Logic level version 2017.10.25.3";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.01.1";
 window.addEventListener('deviceorientation', ondeviceorientation);
 window.addEventListener('devicemotion', ondevicemotion);
 //-------------------------------------------------------
  function ondeviceorientation(event) {
 	document.getElementById("id_alpha").innerHTML = " Alpha = " + Math.round(event.alpha * 10) / 10;
-	document.getElementById("id_beta").innerHTML = " Beta = " + Math.round(event.beta * 10) / 10;
+	document.getElementById("id_beta").innerHTML = " Beta = " + Math.round(event.beta * 10) / 10; 
+	//beta si gama: inclinarea telefonului
+	//alfa-rotatia
 	document.getElementById("id_gamma").innerHTML = " Gamma = " + Math.round(event.gamma * 10) / 10;
 	 
 }
+
+
+//-------------------------------------------------------
+function deseneaza_cerc(gamma,beta)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+
+	context.beginPath();
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+	
+	var centru=(x : w /2, y : h /2);
+	var raza = 10;
+	var max_deplasare_x = w / 2 - raza; 
+	var max_deplasare_y = h / 2 - raza;
+	//gamma == 0 -> centru.x
+	//gamma == -90 -> 0 + raza
+	//gamma = 90 -> w - raza;
+	context.arc(centru.x + gamma / 90 * max_deplasare_x, centru.y + beta / 90 * max_deplasare_y, 10, 0, 2* Math.PI);
+	
+	context.stroke();
+}
+
 //-------------------------------------------------------
 function ondevicemotion(event)
 {
+	deseneaza_cerc(gamma,beta);
+	
 	document.getElementById("id_acc").innerHTML = "Acc = " + Math.round(event.acceleration.x * 10) / 10 + " " + Math.round(event.acceleration.y * 10) / 10 + " " + Math.round(event.acceleration.z * 10) / 10;
 	
 	var ag = event.accelerationIncludingGravity;
