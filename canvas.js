68b6d06d5cb5a3fa4d778cad229734e19f7ef5c5
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-01 18:54:43 +0200

    Am schimbat culoarea la cerc

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML="Logic level version 2017.11.01.2";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.01.2";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -8,8 +8,5 @@ var w = canvas.getAttribute("width");
 var h = canvas.getAttribute("height");
 
 context.arc(w/2, h/2, 10, 0, 2* Math.PI);
-/* context.moveTo(10,10);
-context.lineTo(100,200);
-context.lineTo(200,100);
-context.closePath(); */
+conext.strokeStyle = "red";
 context.stroke();
