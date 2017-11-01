577e1e1b0992fa6351485af604b1a45baeb1118d
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-01 17:55:41 +0200

    cERC

--- canvas.js
+++ canvas.js
@@ -1,11 +1,15 @@
-document.getElementById("id_logic_level_version").innerHTML="Logic level version 2017.11.01.0";
+document.getElementById("id_logic_level_version").innerHTML="Logic level version 2017.11.01.2";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
 context.beginPath();
-context.moveTo(10,10);
+var w = canvas.getAttribute("width");
+var h = canvas.getAttribute("height");
+
+context.arc(w/2, h/2, 10, 0, 2* Math.PI);
+/* context.moveTo(10,10);
 context.lineTo(100,200);
 context.lineTo(200,100);
-context.closePath();
+context.closePath(); */
 context.stroke();
