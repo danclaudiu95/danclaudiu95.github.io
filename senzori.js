72ce65efeaab40cf2f79aaf5c7134a1e8312e099
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-01 19:29:00 +0200

    FunctiaCanvas

--- senzori.js
+++ senzori.js
@@ -10,7 +10,28 @@ window.addEventListener('devicemotion', ondevicemotion);
 	document.getElementById("id_gamma").innerHTML = " Gamma = " + Math.round(event.gamma * 10) / 10;
 	 
 }
+//-------------------------------------------------------
+function deseneaza_patrat_canvas(alfa, gamma, beta)
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
 
+	context.beginPath();
+	var w = canvas.getAttribute("width");
+	var h = canvas.getAttribute("height");
+	
+	context.clearRect(0,0, w, h);
+	var centru=(x : w /2, y : h /2);
+	var latura = 10;
+	var max_deplasare_x = w / 2 - raza; 
+	var max_deplasare_y = h / 2 - raza;
+	//gamma == 0 -> centru.x
+	//gamma == -90 -> 0 + raza
+	//gamma = 90 -> w - raza;
+	//context.arc(centru.x + gamma / 90 * max_deplasare_x, centru.y + beta / 90 * max_deplasare_y, 10, 0, 2* Math.PI);
+	context.strokeRect(centru.x-latura / 2 + gamma / 90 * max_deplasare_x, centru.y - latura / 2 + beta / 90 * max_deplasare_y , latura, latura);
+	context.stroke();
+}
 
 //-------------------------------------------------------
 function deseneaza_cerc_canvas(gamma,beta)
@@ -57,7 +78,8 @@ function deseneaza_cerc_svg(gamma,beta)
 function ondevicemotion(event)
 {
 	//deseneaza_cerc_canvas(gamma,beta);
-	deseneaza_cerc_svg(gamma,beta);
+	//deseneaza_cerc_svg(gamma,beta);
+	deseneaza_patrat_canvas(alfa, gamma, beta);
 	document.getElementById("id_acc").innerHTML = "Acc = " + Math.round(event.acceleration.x * 10) / 10 + " " + Math.round(event.acceleration.y * 10) / 10 + " " + Math.round(event.acceleration.z * 10) / 10;
 	
 	var ag = event.accelerationIncludingGravity;
