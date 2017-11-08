1b229466ee76b5b12f30d2b824f3050b866da41f
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-08 18:13:48 +0200

    touch update

--- touch.js
+++ touch.js
@@ -3,13 +3,25 @@ document.getElementById("id_logic_level_version").innerHTML="Business level vers
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
-window.addEventListener("touchstart", on_touch_start);
+canvas.addEventListener("touchstart", on_touch_start);
+//----------------------------------------------------
+function generate_random_color()
+{
+	var litere = "0123456789ABCDEF";
+	var color="#";
+	for (i=0; i<6; i++)
+		color+=litere[Math.random()];
+}
 
+//----------------------------------------------------
 function on_touch_start(e)
 {
+	var rect = canvas.getBoundingClientRect();
 	var touches = e.changedTouches;
 	for (i=0; i < touches.length, i++)
 		{
-		touches[i].pageX
+		context.beginPath();
+		context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+		context.stroke();
 		}
 }
