52f2cab229af5e67e8938c001c0f18cef9934f6c
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-08 18:39:18 +0200

    3 functii

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.2";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.08.3";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -10,18 +10,35 @@ function generate_random_color()
 	var litere = "0123456789ABCDEF";
 	var color="#";
 	for (i=0; i<6; i++)
-		color+=litere[Math.random()];
+		color+=litere[Math.floor(Math.random() * 16)];
+	return color;
 }
 
 //----------------------------------------------------
 function on_touch_start(e)
 {
-	var rect = canvas.getBoundingClientRect();
 	var touches = e.changedTouches;
 	for (i=0; i < touches.length; i++)
 		{
+		touch_id.push(id:touches[i].identifier, color:generate_random_color());
 		context.beginPath();
 		context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+		context.strokeStyle = touch_id[touch_id.length - 1].color;
+		context.fillStyle = touch_id[touch_id.length - 1].color;
+		context.fill();
 		context.stroke();
 		}
+}
+
+function on_touch_move(e)
+{
+	var touches = e.changedTouches;
+	for(i=0; i < touches.length; i++){
+		context.beginPath();
+		context.arc(touches[i].pageX - rect.left, touches[i].pageY - rect.top, 10, 0, 2 * Math.PI);
+		context.strokeStyle = 
+		context.fillStyle = contex.strokeStyle;
+		context.fill();
+		context.stroke();
+	}
 }
