09cd3bf21649755d0f6d2f61a580b2d4c97911ec
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-15 19:35:15 +0200

    update 1 touch move

--- canvas_move.js
+++ canvas_move.js
@@ -1,12 +1,14 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.15.0";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.15.1";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 var rect_canvas = canvas.getBoundingClientRect();
 var img = document.getElementById(id_img);
-canvas.addEventListener("touchmove", on_touch_move);
+
 
 context.drawImage(img, 100, 100, 100, 100);
+canvas.addEventListener("touchmove", on_touch_move);
+
 //-----------------------------------------------
 function on_touch_move(e)
 
