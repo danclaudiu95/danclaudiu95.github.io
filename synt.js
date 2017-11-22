fcb3ec702af2b3993ac402528f650b9f5c473c5d
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-22 18:52:19 +0200

    Synt bug fix

--- synt.js
+++ synt.js
@@ -6,7 +6,7 @@ var synt = window.speechSynthesis;
 function get_voices()
 {
 	var voices = synt.getVoices();
-	for(var i = 0 < i < voices.length; i++)
+	for(var i = 0; i < voices.length; i++)
 		document.getElementById("id_voices").innerHTML =  voices[i].name + ":" voices[i].lang + "<br>";
 
 //----------------------------------------------------------	
