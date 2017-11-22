8f84b3926250d9dacbd50662bbbabfe9461b457c
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-22 17:54:07 +0200

    Canvas move

--- canvas_move.js
+++ canvas_move.js
@@ -1,14 +1,20 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.15.3";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.0";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 var rect_canvas = canvas.getBoundingClientRect();
 var img = document.getElementById("id_img");
+
+var top_x = 100;
+var top_y = 100;
+
+var img_width = 100;
+var img_height = 100;
+
 img.onload = function(){
-	context.drawImage(img, 100, 100, 100, 100);
+	context.drawImage(img, top_x, top_y, img_width, img_height);
 	var rect_img = img.getBoundingClientRect();
 }
-
 
 canvas.addEventListener("touchmove", on_touch_move);
 
@@ -20,7 +26,13 @@ function on_touch_move(e)
 	var touches = e.changedTouches;
 
 	for (var i = 0; i < touches.length; i++){
-		var circle = touches[i].target;
+		if( touches[i].pageX - rect_canvas.left < top_x + img_width && touches[i].pageY - rect_canvas.left >= top_x &&
+		touches[i].pageY - rect_canvas.top; < top_y + img_height && touches[i].pageY >= top_y - rect_canvas.top;) {
+			context.clearRect();
+			top_x = touches[i].pageX - rect_canvas.left;
+			top_y = touches[i].pageY - rect_canvas.top;
+			context.drawImage(img, top_x, top_y, img_width, img_height);
+		}
 		
 
 	}
