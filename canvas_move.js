f3cfc41982b1bac71a999732ca1a503aadd28761
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-22 18:27:04 +0200

    Update js 2

--- canvas_move.js
+++ canvas_move.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.1";
+document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.2";
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
@@ -11,12 +11,38 @@ var top_y = 100;
 var img_width = 100;
 var img_height = 100;
 
+var offset_inside_image_x;
+var offset_inside_image_y;
+
 img.onload = function(){
 	context.drawImage(img, top_x, top_y, img_width, img_height);
 	var rect_img = img.getBoundingClientRect();
 }
 
 canvas.addEventListener("touchmove", on_touch_move);
+
+//-----------------------------------------------
+function on_touch_start(e)
+
+{
+	e.preventDefault();
+	var touches = e.changedTouches;
+	//touches.length - numarul de apasari/de degete care ating ecranul la un moment dat
+	for (var i = 0; i < touches.length; i++){
+		if( touches[i].pageX - rect_canvas.left < top_x + img_width && touches[i].pageY - rect_canvas.left >= top_x &&
+		touches[i].pageY - rect_canvas.top; < top_y + img_height && touches[i].pageY >= top_y - rect_canvas.top) {
+		    offset_inside_image_x = (touches[i].pageX - rect_canvas.left) - top_x;
+		    offset_inside_image_y = (touches[i].pageY - rect_canvas.top) - top_y;
+			context.clearRect();
+			top_x = touches[i].pageX - rect_canvas.left;
+			top_y = touches[i].pageY - rect_canvas.top;
+			context.drawImage(img, top_x, top_y, img_width, img_height);
+		}
+		
+
+	}
+
+}
 
 //-----------------------------------------------
 function on_touch_move(e)
@@ -28,8 +54,6 @@ function on_touch_move(e)
 	for (var i = 0; i < touches.length; i++){
 		if( touches[i].pageX - rect_canvas.left < top_x + img_width && touches[i].pageY - rect_canvas.left >= top_x &&
 		touches[i].pageY - rect_canvas.top; < top_y + img_height && touches[i].pageY >= top_y - rect_canvas.top) {
-			var offset_inside_image_x = (touches[i].pageX - rect_canvas.left) - top_x;
-			var offset_inside_image_y = (touches[i].pageY - rect_canvas.top) - top_y;
 			context.clearRect();
 			top_x = touches[i].pageX - rect_canvas.left;
 			top_y = touches[i].pageY - rect_canvas.top;
