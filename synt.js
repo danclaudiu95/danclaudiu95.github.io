90e1ea919fcb58ea694807717550744635060eaf
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-28 16:51:51 +0200

    Synt solved

--- synt.js
+++ synt.js
@@ -1,26 +1,26 @@
-document.getElementById("id_logic_level_version").innerHTML="Business level version 2017.11.22.9";
-
-var synt = window.speechSynthesis;
-
-//----------------------------------------------------------
-function get_voices()
-{
-	var voices = synt.getVoices();
-	for(var i = 0; i < voices.length; i++) {
-		var e = document.createElement("option");
-		e.text = voices[i].lang;
-		document.getElementById("id_voices").add(e);
-	}
-}
-//----------------------------------------------------------	
-
-function speak()
-{
+ document.getElementById("id_logic_level_version").innerHTML = "Business level version: 2017.11.22.6"; 
+ 
+ var synt = window.speechSynthesis;
+ 
+ //---------------------------------------
+ function get_voices()
+ {
+	 var voices = synt.getVoices();
+	 for (var i = 0; i < voices.length; i++){
+		 var e = document.createElement("option");
+		 e.text = voices[i].lang;
+		 document.getElementById ("id_voices").add(e);
+	 }
+ }
+ //---------------------------------------
+ function speak()
+ {
 	var enunt = new SpeechSynthesisUtterance();
+	enunt.lang = document.getElementById ("id_voices").value;
 	enunt.text = document.getElementById("id_text").value;
 	
 	enunt.onerror = function(e){
-		alert(e.error)
+		alert(e.error);
 	}
 	
 	enunt.onend = function(e){
@@ -28,7 +28,6 @@ function speak()
 	}
 	
 	document.getElementById("id_button_speak").disabled = true;
-	synt.speak(enunt);
-	
-}
-//----------------------------------------------------------	
+	synt.speak(enunt);	
+ }
+  //---------------------------------------
