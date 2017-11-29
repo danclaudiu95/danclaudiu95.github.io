1912bfcb425ee49b4b52700a339f86e12ee74ae4
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-11-29 19:47:54 +0200

    update alternatives js 2

---
+++ robot.js
@@ -0,0 +1,62 @@
+//touchstart - eveniment ce se activeaza cand atingeam ecranul
+document.getElementById("id_business_level_version").innerHTML = "Business level version: 2017.11.29.2"; 
+
+document.addEventListener("touchstart", on_touch_start);
+
+var recognition = new webkitSpeechRecognition();
+recognition.lang = "en-US";
+recognition.maxAlternatives = 1;
+recognition.onresult = on_speech_result;
+recognition.onsoundend = on_sound_end;
+var is_listening = false;
+//vor rula doar cand iti returneaza google cuvintele recunoscute
+ var synt = window.speechSynthesis;
+ 
+function on_touch_start(e)
+{
+	if(!is_listening) {
+		recognition.start();
+		is_listening = true;
+	}
+}
+
+function on_speech_result(e)
+{
+	var alternatives = e.results[0];
+	for (var i = 0; i < alternatives.length; i++) {
+		document.getElementById("id_p").innerHTML += alternatives[i].transcript + "("+alternatives[i].confidence+")"+"<br>";
+		switch(alternatives[i].transcript){
+			case "go forward":
+				speak("moving forward");
+				break;
+			case "go backward":
+				speak("moving backward");
+				break;
+				//....
+			default:
+				speak("I am studpid!");
+			
+		
+		}
+	}
+}
+
+function on_sound_end(e)
+{
+	recognition.stop();
+	
+}
+
+
+function speak(_text)
+{
+	var enunt = new SpeechSynthesisUtterance();
+	enunt.lang = "en-US"
+	enunt.text = _text;
+	
+	enunt.onend = function(e){
+		is_listening = false;
+	}
+	
+	synt.speak(enunt);	
+}
