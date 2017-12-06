c6e8079bef09b2944a87fad2d963aa7aca123370
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2017-12-06 17:48:53 +0200

    Altitude update 1 js

--- gps.js
+++ gps.js
@@ -1,11 +1,11 @@
-document.getElementById("id_business_level_version").innerHTML = "Business level version: 2017.12.06.0"; 
+document.getElementById("id_business_level_version").innerHTML = "Business level version: 2017.12.06.1"; 
 // fara () se apeleaza doar pointer la functie
 navigator.geolocation.getCurrentPosition(on_gps_success, on_gps_error);
 
 //--------------------------------
 function on_gps_success(p)
 {
-	document.getElementById("id_p").innerHTML = "lat= " +p.coords.latitude+" long = "+p.coords.longitude+" accuracy = "+p.coords.accuracy+ "m";
+	document.getElementById("id_p").innerHTML = "lat= " +p.coords.latitude+"<br> long = "+p.coords.longitude+" <br>accuracy = "+p.coords.accuracy+ "m"+"<br>altitude = "+p.coords.altitude + "m";;
 }
 //--------------------------------
 
