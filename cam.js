562b0c7293f0dcc186cd3a2f5f97027d936ce7c9
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2018-01-10 18:07:28 +0200

    cam1

---
+++ cam.js
@@ -0,0 +1,23 @@
+var d = new Date("July 21, 1983 01:15:00");
+var n = d.getDate();
+
+document.getElementById("id_logic_level_version").innerHTML = 
+	"Business level version: "
+	+d.getFullYear() +"." +d.getMonth() + "1"+"." +d.getDate() +"1"+".1"; 
+
+var constraints = {audio:true, video: true};	
+navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
+
+var video = document.getElementById("id_video");
+//---------------------------------------------------
+function on_success(stream)
+{
+	video.srcObject = stream;
+}
+//---------------------------------------------------
+function on_error(error)
+{
+	alert("Error")
+}
+//---------------------------------------------------
+
