d679018296f0afc8f085572ff4b6c79a9fde5d04
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2018-01-10 19:34:16 +0200

    vib1

--- vibrate.js
+++ vibrate.js
@@ -1,6 +1,19 @@
-document.addEventListener("touchstart", vibrate);
-
+document.addEventListener("touchstart", start_timer);
+document.addEventListener("touchend", stop_timer);
+var timer_id;
+//-------------------------------------------------
+function start_timer()
+{
+	timer_id = setInterval(vibrate, 300);
+}
+//-------------------------------------------------
+function stop_timer()
+{
+	clearInterval(vibrate, 300);
+}
+//-------------------------------------------------
 function vibrate()
 {
-	windows.navigator.vibrate(200);
+	windows.navigator.vibrate(300);
 }
+//-------------------------------------------------
