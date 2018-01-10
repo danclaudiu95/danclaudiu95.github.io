0a434a9e1c9648557d6d16905c7ba18f728d4d02
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2018-01-10 19:52:35 +0200

    light0

---
+++ light.js
@@ -0,0 +1,7 @@
+windows.addEventListener("devicelight", device_light);
+
+function device_light (e)
+{
+	document.getElementById("id_light").innerHTML = 
+		"value = " + e.value + " max = " + e.max;
+}
